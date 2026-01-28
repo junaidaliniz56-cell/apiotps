ca000160562337a96dd0ef8dfdccfdcb0809a291
Junaid Niz <junaidaliniz56@gmail.com>
2026-01-28 08:16:37 +0500

    Update PHPSESSID in request headers

--- api/Kumail.js
+++ api/Kumail.js
@@ -54,7 +54,7 @@ module.exports = async (req, res) => {
     "X-Requested-With": "XMLHttpRequest",
     "Accept-Encoding": "gzip, deflate",
     "Accept-Language": "en-US,en;q=0.9",
-    Cookie: "PHPSESSID=89evjvss93kvtmfdcnue5o538i",
+    Cookie: "PHPSESSID=r3v25jjraavho0k3hbel1tp0j0",
   };
 
   let url;
